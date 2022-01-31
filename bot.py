32d0c3fe3d2376abf7d599c6302bef303578f3ec
Paula Lepore <paula_mpl@hotmail.com>
2022-01-31 10:37:29 +0100

    Set transaction parameters and rsi

--- bot.py
+++ bot.py
@@ -1,9 +1,16 @@
 
-import websocket, json, pprint
+import websocket, json, pprint, talib, numpy
 
 SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
 
+RSI_PERIOD = 14
+RSI_OVERBOUGHT = 70
+RSI_OVERSOLD = 30
+TRADE_SYMBOL = 'ETHUSD'
+TRADE_QUANTITY = 0.009
+
 closes = []
+in_position = False
 
 def on_open(ws):
     print('open connection')
@@ -27,6 +34,25 @@ def on_message(ws, message):
         closes.append(float(close))
         print("closes")
         print(closes)
+
+        if len(closes) > RSI_PERIOD:
+            np_closes = numpy.array(closes)
+            rsi = talib.RSI(np_closes, RSI_PERIOD)
+            print("all rsi calculated")
+            print(rsi)
+            last_rsi = rsi[-1]
+            print("the current rsi is {}".format(last_rsi))
+
+            if last_rsi > RSI_OVERBOUGHT:
+                if in_position:
+                    print("Sell !!!!!!!")
+                else:
+                    print("overbought, but we dont owe any")
+
+            if last_rsi < RSI_OVERSOLD:
+                print("BUYYYY!!")
+                if in_position:
+                    print("it is oversold but you own it, do nothing")
 
 
 ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
